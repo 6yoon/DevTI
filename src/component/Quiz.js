8475cb6940d28c1a50be8ebd13aed50bb1ff0da4
nayun <nayunk10@gmail.com>
2024-08-21 21:24:47 +0900

    header, put logo

--- src/component/Quiz.js
+++ src/component/Quiz.js
@@ -4,9 +4,9 @@ function Quiz() {
     const quiz = [
         {
             id: 1,
-            content: "안녕",
-            type1: "E",
-            type2: "I",
+            content: "프로젝트 회의가 끝나갈 때쯤 나의 모습은?",
+            type1: ["이 아이디어 괜찮은데? 회의 끝나기 전에 꼭 말해야 겠어 ", "E"],
+            type2: ["피곤하다… 회의 끝나고 정리하면 몇 시지?", "I"]
         }
     ];
   return (
