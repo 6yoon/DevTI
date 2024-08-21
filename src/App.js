65d19c533c6d99a3b54879ed73e9a6cf981ea9d0
nayun <nayunk10@gmail.com>
2024-08-21 20:47:49 +0900

    header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Quiz from "./component/Quiz";
+import Header from "./component/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
+      <Quiz></Quiz>
     </div>
   );
 }
---
+++ src/component/Quiz.js
@@ -0,0 +1,19 @@
+import './Quiz.css';
+
+function Quiz() {
+    const quiz = [
+        {
+            id: 1,
+            content: "안녕",
+            type1: "E",
+            type2: "I",
+        }
+    ];
+  return (
+    <div className="Quiz">
+        
+    </div>
+  );
+}
+
+export default Quiz;
